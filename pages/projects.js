36a4d64933e1338feda4ccdeede56c8197187982
Sky <41401030+HeartfulSky@users.noreply.github.com>
2022-08-01 22:49:40 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -38,6 +38,7 @@ export default function Work() {
                                         Featured & recent projects
                                     </p>
                                 </div>
+                                    <div className="w-full grid gap-20 grid-cols-1 md:grid-cols-2">
                                         <div className="flex-grow space-y-2">
                                             <h1 className="text-3xl font-bold">ClumsyHosting</h1>
                                             <p className="text-gray-400 py- text-justify">
@@ -82,6 +83,7 @@ export default function Work() {
                                                 </a>
                                             </div>
                                     </div>
+                                </div>
                             </motion.div>
                         </div>
                     </div>
