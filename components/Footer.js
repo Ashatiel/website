9270a9bb26bc92bdd7fcd8ce46f7410045c626ae
Sky <41401030+HeartfulSky@users.noreply.github.com>
2022-08-01 23:21:58 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -29,16 +29,16 @@ export default function Footer() {
                             <title>Discord</title>
                             <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"/>
                         </svg>
-                        <span className='tooltip bg-gray-800 text-white rounded shadow-lg p-1 -mt-14'>zack;#0997</span>
+                        <span className='tooltip bg-gray-800 text-white rounded shadow-lg p-1 -mt-14'>ash.#0997</span>
                     </a>
-                    <a href="https://twitter.com/skysstw" className="text-gray-400" target="_blank" rel="noopener noreferrer">
+                    <a href="https://twitter.com/ashatiel_" className="text-gray-400" target="_blank" rel="noopener noreferrer">
                         <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5"
                              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>Twitter</title>
                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"/>
                         </svg>
                     </a>
-                    <a href="https://instagram.com/702.zackery" className="text-gray-400" target="_blank" rel="noopener noreferrer">
+                    <a href="https://instagram.com/zackery.lv" className="text-gray-400" target="_blank" rel="noopener noreferrer">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                             className="w-5 h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>Instagram</title>
@@ -46,7 +46,7 @@ export default function Footer() {
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"/>
                         </svg>
                    </a>
-                    <a href="https://twitch.tv/heartfulsky" className="text-gray-400" target="_blank" rel="noopener noreferrer">
+                    <a href="https://twitch.tv/ashatiel_" className="text-gray-400" target="_blank" rel="noopener noreferrer">
                     <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                     <title>Twitch</title>
                     <path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0H3.857zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142v6.286z"/>
