e9ad9801c889c29a9a4ea2db707792c91115d8bd
Ash <41401030+Ashatiel@users.noreply.github.com>
2022-08-13 23:10:54 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,15 @@ export default function Home() {
         <Head>
             <meta charSet="UTF-8"/>
             <title>Zackery.</title>
-            <meta name="description" content="I'VE OFFICIALLY BECAME YOUR BEST FRIEND!" />
+            <meta name="description" content="Just trying my best to do everything I can in life." />
             <meta name="keywords" content="Music,Friends,Family,Singing,Stage,Besties,BestFriends,Personal" />
             <meta name="author" content="Zackery" />
             <link rel="icon" href="/favicon.ico" />
             <meta property="og:title" content="Zackery." />
-            <meta property="og:description" content="I'm trying my best to do everything I can in life." />
+            <meta property="og:description" content="Just trying my best to do everything I can in life." />
             <meta property="og:url" content="https://zackeryjs.xyz/" />
             <meta property="og:image" content="https://zackeryjs.xyz/logo.jpg" />
             <meta name="theme-color" content="#F43F5E" />
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@skyystw" />
-            <meta name="twitter:title" content="Zackery." />
-            <meta name="twitter:description" content="Just your new best friend."/>
-            <meta name="twitter:image" content="https://zackeryjs.xyz/logo.jpg"/>
         </Head>
 
         <div className="flex flex-col min-h-screen">
